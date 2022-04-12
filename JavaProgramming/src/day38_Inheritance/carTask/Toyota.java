fb654397657e26ed5776ae6a07b5d6841683e0fc
Mia <adi_dinica@yahoo.com>
2022-04-12 13:46:08 +0100

    day38_Inheritance-->ToyotaClass

--- JavaProgramming/src/day38_Inheritance/carTask/Toyota.java
+++ JavaProgramming/src/day38_Inheritance/carTask/Toyota.java
@@ -1,4 +1,13 @@
 package day38_Inheritance.carTask;
 
-public class Toyota {
+public class Toyota extends Car{
+
+    public Toyota(String model, String colour, double price, int year, int miles) {
+        super("Toyota", model, colour, price, year, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand + " " + model + " is reliable");
+    }
+
 }
