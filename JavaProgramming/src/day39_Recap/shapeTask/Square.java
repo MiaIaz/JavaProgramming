90b236bedd03723a6a687e9650c0fa792c7839db
Mia <adi_dinica@yahoo.com>
2022-04-13 10:04:08 +0100

    day39_Recap-->MethodOverloading

---
+++ JavaProgramming/src/day39_Recap/shapeTask/Square.java
@@ -0,0 +1,29 @@
+package day39_Recap.shapeTask;
+
+public class Square extends Shape {
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side <= 0){
+            System.err.println("Invalid Side " + side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
+
+
+
+
+
+
+}
