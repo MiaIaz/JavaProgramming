cc8410cd09abe286ad359cfcf215ef0d132efc64
Mia <adi_dinica@yahoo.com>
2022-04-14 20:23:23 +0100

    day40_Recap-->Protected Access Modifier

---
+++ JavaProgramming/src/day40_ProtectedAccessModifier_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,13 @@
+package day40_ProtectedAccessModifier_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1 = "Cydeo"; //default access modifier
+    protected static String name2 = "WoodenSpoon"; //protected
+
+    static void method1(){
+
+    }
+
+
+}
