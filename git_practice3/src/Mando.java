9098ff9b229fe70c7a25f5c0039b1957b24c06e3
Mia <adi_dinica@yahoo.com>
2022-04-17 13:04:34 +0100

    day3Git-TimeMachine->more more work done

--- git_practice3/src/Mando.java
+++ git_practice3/src/Mando.java
@@ -2,5 +2,6 @@ public class Mando {
     public static void main(String[] args) {
         System.out.println("some work done");
         System.out.println("more work done");
+        System.out.println("work is good");
     }
 }
